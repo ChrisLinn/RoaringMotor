8796cb8b97a6272a415162aa45772bf7c5e86c95
ChrisLinn <nanyishanshaoxiu@gmail.com>
2014-12-08 00:11:26 +0800

    基本部署好格局

--- Watson/Compiler/2.c
+++ Watson/Compiler/2.c
@@ -7,55 +7,88 @@
 using namespace std;
 
 #define BUF_SIZE 256
-TCHAR szName[]=TEXT("Global\\MyFileMappingObject");    //指向同一块共享内存的名字
+TCHAR szNameIn[]=TEXT("Global\\DiyIn");
+TCHAR szNameOut[]=TEXT("Global\\DiyOut");    //指向同一块共享内存的名字
 
-int main(int argc, char *argv[])
+int main()
 {
-    HANDLE hMapFile;
-    LPCTSTR pBuf;
-	int i=0;
+    HANDLE hMapFileIn,hMapFileOut;
+    LPCTSTR pBufIn,pBufOut;
 
-    hMapFile = CreateFileMapping(
+    hMapFileIn = CreateFileMapping(
         INVALID_HANDLE_VALUE,    // use paging file
         NULL,                    // default security
         PAGE_READWRITE,          // read/write access
         0,                       // maximum object size (high-order DWORD)
         BUF_SIZE,                // maximum object size (low-order DWORD)
-        szName);                 // name of mapping object
+        szNameIn);                 // name of mapping object
 
-    if (hMapFile == NULL)
+    if (hMapFileIn == NULL)
     {
         _tprintf(TEXT("Could not create file mapping object (%d).\n"),
             GetLastError());
         return 1;
     }
-    pBuf = (LPTSTR) MapViewOfFile(hMapFile,   // handle to map object
+    pBufIn = (LPTSTR) MapViewOfFile(hMapFileIn,   // handle to map object
         FILE_MAP_ALL_ACCESS, // read/write permission
         0,
         0,
         BUF_SIZE);
 
-    if (pBuf == NULL)
+    if (pBufIn == NULL)
     {
         _tprintf(TEXT("Could not map view of file (%d).\n"),
             GetLastError());
 
-        CloseHandle(hMapFile);
+        CloseHandle(hMapFileIn);
+
+        return 1;
+    }
+	
+	
+    hMapFileOut = CreateFileMapping(
+        INVALID_HANDLE_VALUE,    // use paging file
+        NULL,                    // default security
+        PAGE_READWRITE,          // read/write access
+        0,                       // maximum object size (high-order DWORD)
+        2,                // maximum object size (low-order DWORD)
+        szNameOut);                 // name of mapping object
+
+    if (hMapFileOut == NULL)
+    {
+        _tprintf(TEXT("Could not create file mapping object (%d).\n"),
+            GetLastError());
+        return 1;
+    }
+    pBufOut = (LPTSTR) MapViewOfFile(hMapFileOut,   // handle to map object
+        FILE_MAP_ALL_ACCESS, // read/write permission
+        0,
+        0,
+        2);
+
+    if (pBufOut == NULL)
+    {
+        _tprintf(TEXT("Could not map view of file (%d).\n"),
+            GetLastError());
+
+        CloseHandle(hMapFileOut);
 
         return 1;
     }
     //从main开始至此，A B process代码一样，都是获取共享内存的指针
 
-	float* s=(float*)pBuf;
+	double* data=(double*)pBufIn;
+	double* result=(double*)pBufOut;
     //以下代码，不停读写共享内存
     while(1)
 	{
-		printf("%f\n",*++s);
+		printf("%lf\n",*++data);
 		cout<<"A process: hit keyboard to receive from B process"<<endl;
 		getchar();
     }
-
-    UnmapViewOfFile(pBuf);
-    CloseHandle(hMapFile);
+    UnmapViewOfFile(pBufIn);
+    CloseHandle(hMapFileIn);
+    UnmapViewOfFile(pBufOut);
+    CloseHandle(hMapFileOut);
     return 0;
 }
